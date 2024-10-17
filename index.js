321614cb98f1899c2d18af304f36e7ab5a3ca540
Salutah <salutahmoha@gmail.com>
2024-10-17 15:33:20 +0300

    adding the get/read items functionality

--- index.js
+++ index.js
@@ -50,51 +50,51 @@ program
     console.log(chalk.bgBlue("Item added successfully"));
   });
 
-// // Getting Item
-// program
-//   .command("get")
-//   .description("Display all the Items")
-//   .option("-t | --title <value>", "title of the item to be displayed")
-//   .action(function (options) {
-//     const title = options.title;
-//     const loadedItems = fs.readFileSync("./data/item.json", "utf-8");
-//     const items = JSON.parse(loadedItems);
+// Getting Item
+program
+  .command("get")
+  .description("Display all the Items")
+  .option("-t | --title <value>", "title of the item to be displayed")
+  .action(function (options) {
+    const title = options.title;
+    const loadedItems = fs.readFileSync("./data/item.json", "utf-8");
+    const items = JSON.parse(loadedItems);
 
-//     if (items.length === 0) {
-//       console.log(chalk.bgYellow("You do not have any items yet"));
-//       return;
-//     }
+    if (items.length === 0) {
+      console.log(chalk.bgYellow("You do not have any items yet"));
+      return;
+    }
 
-//     if (title) {
-//       const item = items.find((currentItems) => currentItems.title === title);
-//       if (item) {
-//         console.log(item.title);
-//         console.log("--------");
-//         console.log("item.quantity");
-//         console.log("--------");
-//         console.log("item.price");
-//         return;
-//       }
+    if (title) {
+      const item = items.find((currentItems) => currentItems.title === title);
+      if (item) {
+        console.log(item.title);
+        console.log("--------");
+        console.log("item.quantity");
+        console.log("--------");
+        console.log("item.price");
+        return;
+      }
 
-//       console.log(chalk.bgRed(`No notes with title '${title}'`));
-//       return;
-//     }
+      console.log(chalk.bgRed(`No notes with title '${title}'`));
+      return;
+    }
 
-//     items.forEach((currentItems) => {
-//       console.log(chalk.bgBlue("======="));
-//       console.log(currentItems.title);
-//       console.log("--------");
-//       console.log(currentItems.quantity);
-//       console.log("--------");
-//       console.log(currentItems.price);
-//       console.log("--------");
+    items.forEach((currentItems) => {
+      console.log(chalk.bgBlue("======="));
+      console.log(currentItems.title);
+      console.log("--------");
+      console.log(currentItems.quantity);
+      console.log("--------");
+      console.log(currentItems.price);
+      console.log("--------");
 
-//       console.log(
-//         `CreatedAt: ${new Date(currentItems.createdat).toDateString()}`,
-//       );
-//       console.log(chalk.bgBlue("=======\n"));
-//     });
-//   });
+      console.log(
+        `CreatedAt: ${new Date(currentItems.createdat).toDateString()}`,
+      );
+      console.log(chalk.bgBlue("=======\n"));
+    });
+  });
 
 // // Update Item
 // program
