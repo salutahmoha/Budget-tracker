1696076271d54281586e28df0859e99924090b35
Salutah <salutahmoha@gmail.com>
2024-10-17 15:35:53 +0300

    Adding the delete functionality

--- index.js
+++ index.js
@@ -151,36 +151,36 @@ program
   });
 
 
-// // Delete item
-// program
-//   .command("delete")
-//   .description("Delete a specified item")
-//   .option("-t | --title <value>", "title of the item to be deleted")
-//   .action(function (options) {
-//     const title = options.title;
-//     const loadedItems = fs.readFileSync("./data/item.json", "utf-8");
-//     const items = JSON.parse(loadedItems);
+Delete item
+program
+  .command("delete")
+  .description("Delete a specified item")
+  .option("-t | --title <value>", "title of the item to be deleted")
+  .action(function (options) {
+    const title = options.title;
+    const loadedItems = fs.readFileSync("./data/item.json", "utf-8");
+    const items = JSON.parse(loadedItems);
 
-//     if (items.length === 0) {
-//       console.log(chalk.bgYellow("Nothing to delete"));
-//       return;
-//     }
+    if (items.length === 0) {
+      console.log(chalk.bgYellow("Nothing to delete"));
+      return;
+    }
 
-//     const remainingItems = items.filter(
-//       (currentItems) => currentItems.title !== title,
-//     );
-//     if (remainingItems.length === items.length) {
-//       console.log(
-//         chalk.red(
-//           `Could not delete the item with title '${title}'. It doesn't exist`,
-//         ),
-//       );
-//       return;
-//     }
+    const remainingItems = items.filter(
+      (currentItems) => currentItems.title !== title,
+    );
+    if (remainingItems.length === items.length) {
+      console.log(
+        chalk.red(
+          `Could not delete the item with title '${title}'. It doesn't exist`,
+        ),
+      );
+      return;
+    }
 
-//     fs.writeFileSync("./data/item.json", JSON.stringify(remainingItems));
-//     console.log(
-//       chalk.bgGreen(`Notes with title '${title}' deleted successfully`),
-//     );
-//   });
+    fs.writeFileSync("./data/item.json", JSON.stringify(remainingItems));
+    console.log(
+      chalk.bgGreen(`Notes with title '${title}' deleted successfully`),
+    );
+  });
 program.parse(process.argv);
